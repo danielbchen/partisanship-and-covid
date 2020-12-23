29bdc76c5e7f0915e27c686b8dff8b0de7fb9183
danielbchen <danielbchen@uchicago.edu>
2020-12-22 18:39:34 -0600

    Added function to scrape Wikipedia data.

---
+++ covid_partisanship_analysis.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import operator
+import pandas as pd
+import requests
+
+
+def wiki_extractor():
+    '''
+    Scrapes wikipedia table to return a dataframe with the Clinton versus
+    Trump vote counts from 2016.
+    '''
+
+    url = 'https://en.wikipedia.org/wiki/2016_United_States_presidential_election'
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    tables = soup.find_all('table', {'class': 'wikitable sortable'})
+    election_table = tables[2]
+
+    raw_links = []
+    for link in election_table.find_all('a', href=True):
+        raw_links.append(link['href'])
+
+    states = []
+    for link in raw_links:
+        if ('#' not in link):
+            states.append(link)
+
+    td_items = election_table.find_all('td')
+    td_text = [text.get_text() for text in td_items]
+
+    clinton_vote_indexes = []
+    for index, text in enumerate(td_text):
+        if text[0].isupper():
+            clinton_vote_indexes.append(index + 1)
+
+    trump_vote_indexes = []
+    for index, text in enumerate(td_text):
+        if text[0].isupper():
+            trump_vote_indexes.append(index + 4)
+
+    clinton_votes = operator.itemgetter(*clinton_vote_indexes)(td_text) 
+    trump_votes = operator.itemgetter(*trump_vote_indexes)(td_text)
+
+    vote_share_dict = {
+        'STATE': states,
+        'CLINTON_VOTES': clinton_votes,
+        'TRUMP_VOTES': trump_votes,
+    }
+
+    df = pd.DataFrame(vote_share_dict)
+
+    return df
